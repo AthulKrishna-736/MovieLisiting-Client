d0cec68b59c5d591b294d0dcd041809e20052b75
Athul krishna K S <athulkrishna736@gmail.com>
2026-01-11 21:16:41 +0530

    chore: first commit basic project initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    const res = fetch()
+  })
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const CONFIGS = {
+    SERVER_URL: import.meta.env.VITE_SERVER_URL,
+}
